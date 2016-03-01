0b76a4241564f6f26ba7d2a9cbf9790dc10e4201
felmoks <zemtsov.e.e@gmail.com>
2016-03-01 13:24:26 +0000

    Use $. perlvar instead of additional counter $i

--- Intro/big_numbers.sh
+++ Intro/big_numbers.sh
@@ -1,1 +1,1 @@
-perl -anF':' -l0012 -e 'BEGIN { $i = $j = 0 } map { print "$i,$j" if $_ > 10; ++$j } @F; ++$i; $j=0'
+perl -anF':' -l0012 -e 'BEGIN { $j = 0 } map { print $.-1,",$j" if $_ > 10; ++$j } @F; $j=0'
